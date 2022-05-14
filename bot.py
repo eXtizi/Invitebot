5a462bc2628a8e55f8b0d7fc9b57c7456e057377
Pramuditha Vidushan <75202685+eXtizi@users.noreply.github.com>
2022-05-14 22:32:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 invite_logger = -695370268
 esanaChat = -1001764307883
 esanaChannel = -1001608186877
-session=
+session='1AZWarzUBu4oDsHpTyrNHXGaUKcWT9putRU9X0h83ypraGblDHuC8_unMlFudHCCK0MEdUqph2MN3eRoiuHq8kuwETzBJrixXUZ2aKzMSuc7jzPdixfwnQnRXixO7PUcv7a58dm3X_CjxclhSZMTQ5XtNMPkIv5YSHizNDWg46Mz7CokXlDa9-lAtJcQ2Jn8lYUdHwGAXeFCD3aCGwN1rhQ1l4FiNOxr_gXOqOExZGAg0nYY8CUvzU_0tTv_GOIlR2ayNkK6Q1Psqz1nBxJ_Aha9uVu_b0Woa_rxsz2hQRPQO_L56B7-TjlTnO4vDZ3u0bn1kvw2inCJVwpaQA7cnQhedBSbDVJE='
 
 api_id = 8004294
 api_hash = '5091fcf2a3948b6bc7c5f39dec044a63'
